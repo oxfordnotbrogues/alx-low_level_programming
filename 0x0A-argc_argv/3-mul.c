e224b336e5a1794932a85bba3dafda3d09a5506a
oxfordnotbrogues <ifeomaonyeabo33@gmail.com>
2023-06-12 14:47:04 -0700

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -47,7 +47,7 @@ int _atoi(char *s)
  */
 int main(int argc, char *argv[])
 {
-	int result, num 1, num 2;
+	int result, num1, num2;
 
 	if (argc < 3 || argc > 3)
 	{
@@ -55,7 +55,7 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 	num1 = _atoi(argv[1]);
-	num2 = _atoi(arg[2]);
+	num2 = _atoi(argv[2]);
 	result = num1 * num2;
 	printf("%d\n", result);
 	return (0);
