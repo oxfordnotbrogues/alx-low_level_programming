80c2696b70a8cffcdaebf05bcd608fb87ca00788
oxfordnotbrogues <ifeomaonyeabo33@gmail.com>
2023-06-13 15:11:01 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 /**
  * str_concat - get ends of input and add together for size
- * @s1: input one toconcat
+ * @s1: input one to concat
  * @s2: input two to concat
  * Return: concat s1 and s2
  */
