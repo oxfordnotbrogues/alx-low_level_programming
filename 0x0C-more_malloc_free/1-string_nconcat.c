e21c5dc872956e2186a2e5f763312994cd339c3d
oxfordnotbrogues <ifeomaonyeabo33@gmail.com>
2023-06-15 20:14:53 -0700

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,28 +10,26 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	unsigned int s1_len = 0, s2_len = 0, i;
 	char *s;
-	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	while (s1 && s1[len1])
-		len1++;
-	while (s2 && s2[len2])
-		len2++;
-	if (n < len2)
-		s = malloc(sizeof(char) * (len1 + n + 1));
-	else
-		s = malloc(sizeof(char) * (len1 + len2 + 1));
-	if (!s)
+	if (s1 == NULL)
+		s1 = " ";
+	if (s2 == NULL)
+		s2 == " ";
+	while (s1[s1_len] != '\0')
+		s1_len++;
+	while (s2[s2_len] != '\0')
+		s2_len++;
+	if (n >= s2_len)
+		n = s2_len;
+	s = malloc(sizeof(char) * n + s1_len + 1);
+	if (s == NULL)
 		return (NULL);
-	while (i < len1)
-	{
+	for (i = 0; i < s1_len; i++)
 		s[i] = s1[i];
-		i++;
-	}
-	while (n < len2 && 1 < (len1 + n))
-		s[i++] = s2[j++];
-	while (n >= len2 && i < (len1 + len2))
-		s[i++] = s2[j++];
-	s[i] = '\0';
+	for (i = 0; i < n; i++)
+		s[i + s1_len] = s2[i];
+	s[i + s1_len] = '\0';
 	return (s);
 }
