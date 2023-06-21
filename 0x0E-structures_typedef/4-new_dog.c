56ad84d829acf6c466a4748073b618982404b957
oxfordnotbrogues <ifeomaonyeabo33@gmail.com>
2023-06-20 17:46:28 -0700

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -52,7 +52,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	int len1, len2;
 
 	len1 = _strlen(name);
-	len2 = _stlen(owner);
+	len2 = _strlen(owner);
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
