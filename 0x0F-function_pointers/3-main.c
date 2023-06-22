56d53f25479310240b352760067c6e5e96226ce6
oxfordnotbrogues <ifeomaonyeabo33@gmail.com>
2023-06-22 14:47:19 -0700

    task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -25,6 +25,12 @@ int main(int __attribute__((__unused__))
 	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
+		exit(99);
+	}
+	if ((*op == '/' && num2 == 0) ||
+			(*op == '%' && num2 == 0))
+	{
+		printf("Error\n");
 		exit(100);
 	}
 	printf("%d\n", get_op_func(op)(num1, num2));
