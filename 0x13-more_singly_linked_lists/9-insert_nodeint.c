c2bac6153a25302ed4de36c92b20068146aecc1d
oxfordnotbrogues <ifeomaonyeabo33@gmail.com>
2023-07-06 15:13:35 -0700

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -7,7 +7,7 @@
  * @n: data to insert the new code
  * Return: pointer to the new node or NULL
  */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
 	listint_t *new;
