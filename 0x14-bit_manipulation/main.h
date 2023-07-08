cdfcfcf85b0b60190bfe20ec27f69ed67bdddac7
oxfordnotbrogues <ifeomaonyeabo33@gmail.com>
2023-07-08 15:04:56 -0700

    main.h corrected

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -8,5 +8,5 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
-unt _atoi(const char *s);
+int _atoi(const char *s);
 #endif
