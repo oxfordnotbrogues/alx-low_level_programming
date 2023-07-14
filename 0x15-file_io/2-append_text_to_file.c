a6f59611235da32c4210e0d4d75404334b8f8b41
oxfordnotbrogues <ifeomaonyeabo33@gmail.com>
2023-07-14 12:07:31 -0700

    task2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,7 +18,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		for (len = 0; text_content[len];)
 			len++;
 	}
-	0 = open(filename, O_WRONLY | O_APPEND);
+	o = open(filename, O_WRONLY | O_APPEND);
 	w = write(o, text_content, len);
 	if (o == -1 || w == -1)
 		return (-1);
